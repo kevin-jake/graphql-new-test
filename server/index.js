919746d4bb0fdaeb78d599458f0c8ee534624698
Kevin Jake <kevin_jake_04@yahoo.com>
2022-09-14 15:12:32 +0800

    add app baseline

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
 const connectDB = require('./config/db')
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 5000
 
 const app = express()
@@ -12,6 +13,8 @@ const app = express()
 connectDB();
 
 app.use(cors())
+app.use(bodyParser.json())
+
 
 app.use('/graphql', graphqlHTTP({
     schema,
