1ed04ddf010cec80397e88057c92d4d90325678a
Kevin Jake <kevin_jake_04@yahoo.com>
2022-09-17 20:56:27 +0800

    add comment and like functionality

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -53,5 +53,26 @@ module.exports = {
         throw new Error(err);
       }
     },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          // Post already like just unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
   },
 };
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,12 +6,27 @@ module.exports = gql`
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
   }
   type User {
     id: ID!
     username: String!
     email: String!
     token: String!
+    createdAt: String!
+  }
+  type Comment {
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
+  }
+  type Like {
+    id: ID!
+    email: String!
+    token: String!
+    username: String!
     createdAt: String!
   }
   input RegisterInput {
@@ -29,5 +44,8 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
