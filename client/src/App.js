ffce03640977b72676c73c7cf1375472dd7b986f
Kevin Jake <kevin_jake_04@yahoo.com>
2022-09-18 13:04:07 +0800

    add menu bar functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "semantic-ui-css/semantic.css";
+import { Container } from "semantic-ui-react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -8,14 +9,16 @@ import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <MenuBar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <Container>
+      <BrowserRouter>
+        <MenuBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 
--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,36 +1,41 @@
 import React, { useState } from "react";
 import { Input, Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const MenuBar = () => {
-  const [activeItem, setActiveItem] = useState("");
+  const pathName = window.location.pathname;
+  const path = pathName === "/" ? "home" : pathName.substring(1);
+  const [activeItem, setActiveItem] = useState(path);
 
-  const handleItemClick = (e, { name }) => setActiveItem({ activeItem: name });
+  const handleItemClick = (e, { name }) => setActiveItem(name);
 
   return (
-    <Menu secondary>
+    <Menu secondary size="massive" color="teal">
       <Menu.Item
         name="home"
         active={activeItem === "home"}
         onClick={handleItemClick}
+        as={Link}
+        to="/"
       />
-      <Menu.Item
-        name="messages"
-        active={activeItem === "messages"}
-        onClick={handleItemClick}
-      />
-      <Menu.Item
-        name="friends"
-        active={activeItem === "friends"}
-        onClick={handleItemClick}
-      />
+
       <Menu.Menu position="right">
         <Menu.Item>
           <Input icon="search" placeholder="Search..." />
         </Menu.Item>
         <Menu.Item
-          name="logout"
-          active={activeItem === "logout"}
+          name="login"
+          active={activeItem === "login"}
           onClick={handleItemClick}
+          as={Link}
+          to="/login"
+        />
+        <Menu.Item
+          name="register"
+          active={activeItem === "register"}
+          onClick={handleItemClick}
+          as={Link}
+          to="/register"
         />
       </Menu.Menu>
     </Menu>
