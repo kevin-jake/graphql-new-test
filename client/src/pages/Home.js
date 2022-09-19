661c835418aa94b691d35eb9b2ca118e3843fb20
Kevin Jake <kevin_jake_04@yahoo.com>
2022-09-19 16:31:51 +0800

    add register ui and errors

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,55 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import { Grid } from "semantic-ui-react";
+import PostCards from "../components/PostCards";
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 const Home = () => {
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+  var posts = [];
+  if (data) {
+    posts = data.getPosts;
+  }
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <Grid columns={3}>
+      <Grid.Row className="mt-2">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts...</h1>
+        ) : (
+          posts.length > 0 &&
+          posts.map((post) => (
+            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+              <PostCards post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+      <Grid.Row></Grid.Row>
+    </Grid>
   );
 };
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,117 @@
-import React from "react";
+import { gql, useMutation } from "@apollo/client";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Form } from "semantic-ui-react";
+import { useForm } from "../util/hooks";
 
-const Register = () => {
-  return <div>Register</div>;
+const Register = (props) => {
+  const initialState = {
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+  const { onChange, onSubmit, values } = useForm(registerUser, initialState);
+
+  const [addUser, { loading }] = useMutation(REGISTER_USER, {
+    update(_, result) {
+      console.log(result);
+      navigate("/");
+    },
+    onError(err) {
+      console.log(err.graphQLErrors[0].extensions.errors);
+      setErrors(err.graphQLErrors[0].extensions.errors);
+    },
+    variables: values,
+  });
+
+  function registerUser() {
+    addUser();
+  }
+
+  return (
+    <div>
+      <Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ""}>
+        <h1>Register</h1>
+        <Form.Input
+          label="Username"
+          placeholder="Username.."
+          name="username"
+          type="text"
+          value={values.username}
+          onChange={onChange}
+          error={errors.username ? true : false}
+        ></Form.Input>
+        <Form.Input
+          label="Email"
+          placeholder="Email.."
+          name="email"
+          type="text"
+          value={values.email}
+          onChange={onChange}
+          error={errors.email ? true : false}
+        ></Form.Input>
+        <Form.Input
+          label="Password"
+          placeholder="Password.."
+          name="password"
+          type="password"
+          value={values.password}
+          onChange={onChange}
+          error={errors.password ? true : false}
+        ></Form.Input>
+        <Form.Input
+          label="Confirm Password"
+          placeholder="Confirm Password.."
+          name="confirmPassword"
+          type="password"
+          value={values.confirmPassword}
+          onChange={onChange}
+          error={errors.confirmPassword ? true : false}
+        ></Form.Input>
+        <Button type="submit" primary>
+          Register
+        </Button>
+      </Form>
+      {errors
+        ? Object.keys(errors).length > 0 && (
+            <div className="ui error message">
+              <ul className="list">
+                {Object.values(errors).map((value) => (
+                  <li key={value}>{value}</li>
+                ))}
+              </ul>
+            </div>
+          )
+        : ""}
+    </div>
+  );
 };
+
+const REGISTER_USER = gql`
+  mutation register(
+    $username: String!
+    $email: String!
+    $password: String!
+    $confirmPassword: String!
+  ) {
+    register(
+      registerInput: {
+        username: $username
+        email: $email
+        password: $password
+        confirmPassword: $confirmPassword
+      }
+    ) {
+      id
+      email
+      username
+      createdAt
+      token
+    }
+  }
+`;
 
 export default Register;
