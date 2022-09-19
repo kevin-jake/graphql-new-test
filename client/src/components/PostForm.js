2d6828706a49a344d7051d7037ab1e92ff25cd8e
Kevin Jake <kevin_jake_04@yahoo.com>
2022-09-19 22:30:22 +0800

    add caching and create post

---
+++ client/src/components/PostForm.js
@@ -0,0 +1,73 @@
+import { gql, useMutation } from "@apollo/client";
+import React from "react";
+import { Button, Form } from "semantic-ui-react";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
+import { useForm } from "../util/hooks";
+
+const PostForm = () => {
+  const { values, onChange, onSubmit } = useForm(createPostCallback, {
+    body: "",
+  });
+
+  const [createPost, { error }] = useMutation(CREATE_POST, {
+    variables: values,
+    update(proxy, result) {
+      const data = proxy.readQuery({
+        query: FETCH_POSTS_QUERY,
+      });
+      proxy.writeQuery({
+        query: FETCH_POSTS_QUERY,
+        data: {
+          getPosts: [result.data.createPost, ...data.getPosts],
+        },
+      });
+      values.body = "";
+    },
+  });
+
+  function createPostCallback() {
+    createPost();
+  }
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <h2>Create a post:</h2>
+      <Form.Field>
+        <Form.Input
+          placeholder="Hi World!"
+          name="body"
+          type="text"
+          onChange={onChange}
+          value={values.body}
+        ></Form.Input>
+        <Button type="submit" color="teal">
+          Submit
+        </Button>
+      </Form.Field>
+    </Form>
+  );
+};
+
+const CREATE_POST = gql`
+  mutation createPost($body: String!) {
+    createPost(body: $body) {
+      id
+      body
+      createdAt
+      username
+      likes {
+        id
+        username
+        createdAt
+      }
+      comments {
+        id
+        body
+        username
+        createdAt
+      }
+    }
+  }
+`;
+
+export default PostForm;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,42 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useQuery, gql } from "@apollo/client";
 import { Grid } from "semantic-ui-react";
-import PostCards from "../components/PostCards";
 
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`;
+import PostCards from "../components/PostCards";
+import { AuthContext } from "../context/auth";
+import PostForm from "../components/PostForm";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
 
 const Home = () => {
+  const { user } = useContext(AuthContext);
   const { loading, data } = useQuery(FETCH_POSTS_QUERY);
   var posts = [];
   if (data) {
     posts = data.getPosts;
   }
+
   return (
     <Grid columns={3}>
       <Grid.Row className="mt-2">
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
+        <Grid.Row>
+          {user && (
+            <Grid.Column>
+              <PostForm />
+            </Grid.Column>
+          )}
+        </Grid.Row>
         {loading ? (
           <h1>Loading posts...</h1>
         ) : (
